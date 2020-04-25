143e6ecb1213b82513a51964a333f70787097ed7
yandashuaige <y94j06@163.com>
2020-04-25 15:24:20 +0800

    测试案例

---
+++ JavaEE-demo/src/main/java/com/atguigu/javaee/GrammarDemo.java
@@ -0,0 +1,86 @@
+package com.atguigu.javaee;
+
+import java.util.Scanner;
+
+/**
+ * @author Maple
+ * @create 2020-04-21 21:56
+ */
+public class GrammarDemo {
+
+    public static void main(String[] args) {
+//        byte b = 3;
+////        b = b + 4;
+//        b = (byte)(b+4);
+//
+//        char c = 'a';
+//        int  i = 5;
+//        float d = .314F;
+//        double result = c+i+d;
+//        System.out.println(result);
+//
+//        byte f = 5;
+//        short s = 3;
+////        short t = s + f;
+//
+//        String str1 = "4";
+////        String str2 = 4;
+//
+//        String str2 = 3.5f + "";
+//        System.out.println(str2);
+//        System.out.println('a' + 1 + "hello");
+//        System.out.println("hello" + 'a' + 1);
+//
+//        int i1 = 10;int i2 = 20;
+//        int i = i1++;
+//        System.out.print("i="+i);//10
+//        System.out.println("i1="+i1);//11
+//        i = ++i1;
+//        System.out.print("i="+i);//12
+//        System.out.println("i1="+i1);//12
+//        i = i2--;
+//        System.out.print("i="+i);//20
+//        System.out.println("i2="+i2);//19
+//        i = --i2;
+//        System.out.print("i="+i);//18
+//        System.out.println("i2="+i2);//18
+//
+//        short s =3;
+////        s = s + 3;
+//        s += 3;
+//
+//        int i = 1;
+//        i *= 0.1;
+//        System.out.println(i);//0
+//        i++;
+//        System.out.println(i);//1
+//
+//        boolean x=true;
+//        boolean y=false;
+//        short z=42;
+//        //if(y = true)
+//        if((z++==42)&&(y=true))z++;
+//        if((x=false) || (++z==45))z++;
+//        System. out.println("z="+z);
+//
+//        Scanner in1 = new Scanner(System.in);
+//        int i = in1.nextInt();
+//        System.out.println("1 = " + i);
+//
+//
+//        for (int i = 1; i <= 4; i++) {
+//            if (i > 1){
+//                for (int j = 0; j < (2 * i - 1); j++) {
+//                    System.out.print("*\t");
+//                }
+//            }else {
+//                System.out.print("*");
+//            }
+//
+//            System.out.println();
+//        }
+
+    }
+
+
+}
